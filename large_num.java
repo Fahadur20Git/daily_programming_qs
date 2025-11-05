26f8b17969e264859c31e6bd63821b7046d958ed
Fahadur20Git <fahadur516@gmail.com>
2025-11-05 17:44:44 +0530

    question 1

--- large_num.java
+++ large_num.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class large_num {
-    
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        if(a>=b)
+        {
+            System.out.println("a is greater");
+        }else{
+            System.out.println("b is greater");
+        }
+
+    }
 }
