f18e15f68f6e352f4620bfe135ea143b73efdab5
Fahadur20Git <fahadur516@gmail.com>
2025-11-06 23:36:13 +0530

    question 8

---
+++ Palindrome.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--)
+            rev += str.charAt(i);
+
+        if (str.equals(rev))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
