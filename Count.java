3f726582a396a18a17929ed9cb9cc7d9002ebed3
Fahadur20Git <fahadur516@gmail.com>
2025-11-06 23:53:46 +0530

    question 9

---
+++ Count.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Count {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextInt();
+        System.out.println(String.valueOf(n).length());
+    }
+}
