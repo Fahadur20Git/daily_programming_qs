3f29775f403e3d325e5abe939b9fb36bcdb8b96a
Fahadur20Git <fahadur516@gmail.com>
2025-11-06 23:28:29 +0530

    question 6

--- large_num.java
+++ large_num.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class large_num {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
+        System.out.println("enter a");
         int a = scan.nextInt();
+        System.out.println("enter b");
         int b = scan.nextInt();
 
         if(a>=b)
