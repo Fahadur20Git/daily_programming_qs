4d8b95481690a387166d18f000ebaee2e23e7253
Fahadur20Git <fahadur516@gmail.com>
2025-11-09 22:47:04 +0530

    question 17

---
+++ missingele.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class missingele {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+        System.out.println("Enter the numbers:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int n = size + 1;  
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+        for (int i = 0; i < size; i++) {
+            actualSum += arr[i];
+        }
+        int missing = expectedSum - actualSum;
+        System.out.println("Missing number: " + missing);
+    }
+}
