a9b0854854a7b03803a92f76f70cf61fcf50b50f
Fahadur20Git <fahadur516@gmail.com>
2025-11-24 16:01:26 +0530

    Question 34

---
+++ alphapyra.java
@@ -0,0 +1,19 @@
+public class alphapyra {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int s = 1; s <= n - i; s++)
+                System.out.print(" ");
+
+            for (int j = 1; j <= i; j++)
+                System.out.print((char)('A' + j - 1));
+
+            for (int j = i - 1; j >= 1; j--)
+                System.out.print((char)('A' + j - 1));
+
+            System.out.println();
+        }
+    }
+}
