e57897727ff88edbf04e87ee96f0bccd397b56f6
Fahadur20Git <fahadur516@gmail.com>
2025-11-24 15:50:06 +0530

    Question 31
    Implement pyramid pattern printing in Java.

---
+++ hlwpyramid.java
@@ -0,0 +1,24 @@
+public class hlwpyramid {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i < n; i++) {
+
+            for (int s = 1; s <= n - i; s++)
+                System.out.print(" ");
+
+            System.out.print("*");
+
+            for (int j = 1; j <= (2*i - 3); j++)
+                System.out.print(" ");
+
+            if (i > 1)
+                System.out.print("*");
+
+            System.out.println();
+        }
+
+        for (int i = 1; i <= (2*n - 1); i++)
+            System.out.print("*");
+    }
+}
